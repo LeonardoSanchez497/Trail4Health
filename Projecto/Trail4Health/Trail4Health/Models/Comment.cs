abc9df9d4000081ab857673523168251bb622358
Leonardo Sanchez <32192522+LeonardoSanchez497@users.noreply.github.com>
2017-11-17 23:31:47 +0000

    Comentar Experiencia do Trilho 1- desenvolver a pagina para inserir o comentario (formulario) 2- tentativa de criar a pagina de visualizacao do formulario
    Nota: Os ficheiros foram apagados misteriosamente , mas este sao os mesmos que foram realizados na durante a aula de Programacao para a Internet

--- Projecto/Trail4Health/Trail4Health/Models/Comment.cs
+++ Projecto/Trail4Health/Trail4Health/Models/Comment.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Trail4Health.Models
 {
-    public class Comment
-    {
-        //Comentar
-        [Required (ErrorMessage = "Please enter your comment")]
+    //Comentar
+    public class comment {
+        [Required(ErrorMessage = "Please enter your comment")]
         public string comentario { get; set; }
+         [Required(ErrorMessage = "Please enter your Name")]
+        public string nome { get; set; }
+         [Required(ErrorMessage = "Please enter Trilho Name ")]
+         public string nometrilho { get; set; }
+ 
+         [Required(ErrorMessage = "Please enter if you finish or not")]
+         public bool? completou { get; set; }
+         
+         [Required(ErrorMessage = "Please enter time spend on Trilho")]
+        public string tempo { get; set; }
+
+         [Required(ErrorMessage = "Please enter your motive to quit")]
+         public string motivo { get; set; }
     }
-}
+    
+ }
