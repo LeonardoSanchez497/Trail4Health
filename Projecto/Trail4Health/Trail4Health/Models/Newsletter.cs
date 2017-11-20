6ef371fbe64c1d319d04684cf9cf01621d61730b
FabioMartins <fabio.anjos.martins@gmail.com>
2017-11-20 15:15:37 +0000

    Newsletter
    -Add página Newsletter;
    -Add Digito de Controlo

---
+++ Projecto/Trail4Health/Trail4Health/Models/Newsletter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trail4Health.Models
+{
+    public class Newsletter
+    {
+        [Required(ErrorMessage = " Enviar mail para:")]
+        public string Para { get; set; }
+
+        [Required(ErrorMessage = " Introduza um assunto")]
+        public string Assunto { get; set; }
+
+        [Required(ErrorMessage = " Introduza uma mensagem")]
+        public string Mensagem { get; set; }
+
+        [Required(ErrorMessage = " submeter")]
+        public bool? Enviar { get; set; }
+    }
+}
+
