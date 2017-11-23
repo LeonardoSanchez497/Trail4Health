fddf7bbbc85e74982bd6843478832a10b43d08df
Leonardo Sanchez <32192522+LeonardoSanchez497@users.noreply.github.com>
2017-11-23 17:13:43 +0000

    Atualização
    - Atualizacao da versao 1.1 do core para 2.0
    - Implementacao do diagrama de classes
    - formularios e classes adjacentes ao diagrama de classes

---
+++ Trail4Healthtest/Controllers/ComentariosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Trail4Healthtest.Data;
+using Trail4Healthtest.Models;
+
+namespace Trail4Healthtest.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private readonly TrailsDbContext _context;
+
+        public ComentariosController(TrailsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comentarios
+        public async Task<IActionResult> Index()
+        {
+            var trailsDbContext = _context.Comentarios.Include(c => c.turista);
+            return View(await trailsDbContext.ToListAsync());
+        }
+
+        // GET: Comentarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios
+                .Include(c => c.turista)
+                .SingleOrDefaultAsync(m => m.comentarioId == id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Create
+        public IActionResult Create()
+        {
+            ViewData["turistaId"] = new SelectList(_context.Turista, "turistaId", "nome");
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("comentarioId,turistaId,trilhoId,duracaoTrilho,avaliacao,comentar")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comentarios);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["turistaId"] = new SelectList(_context.Turista, "turistaId", "nome", comentarios.turistaId);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios.SingleOrDefaultAsync(m => m.comentarioId == id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+            ViewData["turistaId"] = new SelectList(_context.Turista, "turistaId", "nome", comentarios.turistaId);
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("comentarioId,turistaId,trilhoId,duracaoTrilho,avaliacao,comentar")] Comentarios comentarios)
+        {
+            if (id != comentarios.comentarioId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comentarios);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComentariosExists(comentarios.comentarioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["turistaId"] = new SelectList(_context.Turista, "turistaId", "turistaId", comentarios.turistaId);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios
+                .Include(c => c.turista)
+                .SingleOrDefaultAsync(m => m.comentarioId == id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comentarios = await _context.Comentarios.SingleOrDefaultAsync(m => m.comentarioId == id);
+            _context.Comentarios.Remove(comentarios);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComentariosExists(int id)
+        {
+            return _context.Comentarios.Any(e => e.comentarioId == id);
+        }
+    }
+}
---
+++ Trail4Healthtest/Models/Newsletter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trail4Healthtest.Models
+{
+    public class Newsletter
+    {
+        public int newletterId { get; set; } //Pk
+        public DateTime data { get; set; }
+        public string descricao { get; set; }
+        public Byte foto { get; set; } //array de bytes
+
+    }
+}
