e429a92d53f15848ad6501bafaa51906070d4be4
FabioMartins <fabio.anjos.martins@gmail.com>
2018-01-18 18:03:38 +0000

    -AtualizaçaoBD
    AtualizaçaoBD

--- Trail4Healthtest/Models/Newsletter.cs
+++ Trail4Healthtest/Models/Newsletter.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Trail4Healthtest.Models
 {
     public partial class Newsletter
     {
         public int NewletterId { get; set; }
+
+        [Display(Name = "Data Criação")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}", ApplyFormatInEditMode = false)]
+        [DataType(DataType.DateTime)]
         public DateTime Data { get; set; }
+
+        [StringLength(5000)]
         public string Descricao { get; set; }
         public byte Foto { get; set; }
         public int? TrilhoId { get; set; }
